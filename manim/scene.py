0fb60ebfc3d8adb62df13bf89a0fb9eb5b80acc6
Exr0n <mail@exr0n.com>
2021-06-08 11:55:28 -0700

    finally spacing is right (forgot to change some semantics)

--- manim/scene.py
+++ manim/scene.py
@@ -7,58 +7,101 @@ POEM = [
 #                        \/ duration, None = (until next - 0.5)
 #                              \/ onscreen length (number of lines remaining to stay onscreen)
 #                                  \/ text
-[
-    ( None,  0.5,  1.00, None, 0, "In the beginning," ),
-    ( None,  0.5,  3.00, None, 0, "it was" ),
-    ( None,  0.5,  5.00, None, 0, "chaotic." ),
-    ( None, -0.5,  7.00, None, 0, "A stew" ),
-    ( None, -0.5,  8.00, None, 1, "almost" ),
-    ( None, -0.5,  9.00, None, 0, "conscious." ),
-],
-[
-    ( None,  0.5,  1.00, None, 0, "Perception," ),
-    ( None,  0.5,  3.00, None, 0, "only of the present." ),
-    ( None, -0.5,  7.00, None, 0, "An ape" ),
+# [
+#     ( None,  0.5,  1.00, None, 0, "In the beginning," ),
+#     ( None,  0.5,  3.00, None, 0, "it was" ),
+#     ( None,  0.5,  5.00, None, 0, "chaotic." ),
+#     ( None, -0.5,  7.00, None, 0, "A stew" ),
+#     ( None, -0.5,  8.00, None, 1, "almost" ),
+#     ( None, -0.5,  9.00, None, 0, "conscious." ),
+# ],
+# [
+#     ( None,  0.5,  1.00, None, 0, "Perception," ),
+#     ( None,  0.5,  3.00, None, 0, "only of the present." ),
+#     ( None, -0.5,  7.00, None, 0, "An ape" ),
+#     None,
+#     ( None, -0.5,  9.00, None, 0, "human." ),
+# ],
+#         ]
+
+[ ( None, 0.5 ), [
+    (   1.00, None, 1, "In the beginning," ),
+    (   3.00, None, 1, "it was" ),
+    (   5.00, None, 1, "chaotic." ),
+]],
+[ ( None, -0.5 ), [
+    (   7.00, None, 0, "A stew" ),
+    (   8.00, None, 2, "almost" ),
+    (   9.00, None, 0, "conscious." ),
+]],
+[ ( None, 0.5 ), [
+    (   1.00, None, 1, "Perception," ),
+    (   3.00, None, 1, "only of the present." ),
+]],
+[ ( None, -0.5 ), [
+    (   7.00, None, 0, "An ape" ),
     None,
-    ( None, -0.5,  9.00, None, 0, "human." ),
-],
-        ]
+    (   9.00, None, 0, "human." ),
+]],
+]
 
 KEARN_GAP = 0.4
+CHAR_HEIGHT = 0.8
 COMMA_HEIGHT = 0.12
+
+def is_shifted(s):
+    for char in ',;yp':
+        if char in s:
+            return True
+    return False
 
 class textTest(Scene):
     def construct(self):
         onscreen = []
 
-        for line in POEM:
+        for keyopt, line in POEM:
             # calculate edge cases
-            yoffset = [ int("," in val or ";" in val) for val in line if val is not None ]
+            yoffset = [ int(is_shifted(val[-1])) if val is not None else 0 for val in line ]
             keystones = [ i for i,val in enumerate(line) if val is None ]
-            print(keystones)
 
             # create the base
-            if len(onscreen) > 0:
-                anims = [ obj[1] for obj in onscreen ]
+            # if len(onscreen) > 0:
+            #     anims = [ obj[1] for obj in onscreen ]
+            # else:
+            anims = [Text("e", color=BLUE)]
+            keyobj = anims[0]
+            # x shift
+            if keyopt[0] is None:
+                keyobj.to_edge(LEFT)
             else:
-                anims = [Text("e", color=BLACK)]
-            anims[0].to_edge(LEFT)
+                keyobj.shift([KEARN_GAP*keyopt[0], 0, 0])
+            # y shift
+            if keyopt[1] is not None:
+                anims[0].shift([0, CHAR_HEIGHT*keyopt[1], 0])
 
+            last = False
             for val, toshift in zip(line, yoffset):
+                print(val, toshift)
                 if val is None: continue
                 anims.append(Text(val[-1], color=WHITE)
                         .next_to(anims[-1], RIGHT, buff=KEARN_GAP)
-                        .align_to(anims[-1], DOWN)
-                        .shift([0, -COMMA_HEIGHT*toshift, 0]))
+                        .align_to(keyobj, DOWN)
+                        .shift([0, -COMMA_HEIGHT*toshift, 0])
+                        )
 
                 onscreen.append([val[-2], anims[-1]])
 
             for anim in anims:
                 self.play(Create(anim))
 
+            self.wait(1)
+
             for obj in onscreen:
                 obj[0] -= 1
-            self.play(*[ FadeOut(obj[1]) for obj in onscreen if obj[0] < 0 ])
+            try:
+                self.play(*[ FadeOut(obj[1]) for obj in onscreen if obj[0] < 0 ])
+            except ValueError:
+                pass
             onscreen = [ obj for obj in onscreen if obj[0] >= 0 ]
 
 
